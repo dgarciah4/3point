c56eeafd5ee54c03b617a8e8e8d4ebf485ceb03c
dgarciah4 <diego-4444@live.com>
2023-05-17 17:53:31 -0500

    sample database connect file and updated logo

--- 3point-alpha/src/App.js
+++ 3point-alpha/src/App.js
@@ -22,4 +22,30 @@ function App() {
   );
 }
 
+
+/*  CONNECTING TO DATABASE
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://3pointdevs:<password>@cluster0.kmhin23.mongodb.net/?retryWrites=true&w=majority";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+*/
 export default App;
